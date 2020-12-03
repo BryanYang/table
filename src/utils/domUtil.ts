9b4cae6dbe78743e0340aaba87b41d1f34b810ea
杨高飞 <yanggaofei@meituan.com>
2020-12-03 11:18:11 +0800

    fix(lint): lint

--- src/utils/domUtil.ts
+++ src/utils/domUtil.ts
@@ -1,23 +1,24 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-expressions */
 // 获取第一个可以滚动的父元素
 export function getScrollParent(node: Element) {
   if (node == null) {
     return null;
   }
 
-  if(node === document.body) {
+  if (node === document.body) {
     return node;
   }
 
   if (node.scrollTop > 0) {
     return node;
   }
-  
+
   node.scrollTop += 1;
   const top = node.scrollTop;
   top && (node.scrollTop = 0);
-  if(top > 0) {
+  if (top > 0) {
     return node;
-  } 
+  }
   return getScrollParent(node.parentElement);
-
 }
